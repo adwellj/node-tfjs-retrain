f72c9e8381bdd90f52cfa22b14ab9e34e1de405a
adwellj <43648708+adwellj@users.noreply.github.com>
2018-12-16 17:08:50 -0500

    Augmenting Brightness. Reading images and parsing path at the start instead of in each augment.

--- augment_images.js
+++ augment_images.js
@@ -26,41 +26,74 @@ async function run() {
         .split(";")
         .map(item => item.toLowerCase());
 
-    console.time("Flipping");
     for (const item of images) {
+        const files = await Promise.all(
+            item.images.map(async name => {
+                return {
+                    image: await cv.imreadAsync(name),
+                    parsed: path.parse(name)
+                };
+            })
+        );
+
         if (!labels_to_skip.includes(item.label.toLowerCase())) {
             if (args.flip_images) {
-                const newNames = await augment_flip(item.images, true, true);
+                const newNames = await augment_flip(files, true, true);
+            }
+            if (args.adjust_brightness) {
+                const newNames = await augment_brightness(files);
             }
         }
     }
-    console.timeEnd("Flipping");
 }
 
-async function augment_flip(fileNames, flip_x, flip_y) {
+async function augment_brightness(files, brightnessDelta = 0.2) {
     let names = [];
 
-    for (const item of fileNames) {
-        const image = await cv.imreadAsync(item);
-        const parsed = path.parse(item);
-        const baseDir = path.join(parsed.dir, "flipped");
-        const baseName = path.join(baseDir, parsed.name);
+    for (const item of files) {
+        const baseDir = path.join(item.parsed.dir, "brightness");
+        const baseName = path.join(baseDir, item.parsed.name);
+
+        let writePromises = [];
+        await fse.ensureDir(baseDir);
+
+        const brighter = item.image.mul(1 + brightnessDelta);
+        const darker = item.image.mul(1 - brightnessDelta);
+        names.push(baseName + "_b" + item.parsed.ext);
+        writePromises.push(cv.imwriteAsync(names[names.length - 1], brighter));
+        names.push(baseName + "_d" + item.parsed.ext);
+        writePromises.push(cv.imwriteAsync(names[names.length - 1], darker));
+
+        await Promise.all(writePromises);
+    }
+}
+
+async function augment_flip(files, flip_x, flip_y) {
+    let names = [];
+
+    for (const item of files) {
+        const baseDir = path.join(item.parsed.dir, "flipped");
+        const baseName = path.join(baseDir, item.parsed.name);
 
         let writePromises = [];
         if (flip_x || flip_y) await fse.ensureDir(baseDir);
 
         if (flip_x) {
-            names.push(baseName + "_m_x" + parsed.ext);
-            writePromises.push(flipAndWrite(image, 1, names[names.length - 1]));
+            names.push(baseName + "_m_x" + item.parsed.ext);
+            writePromises.push(
+                flipAndWrite(item.image, 1, names[names.length - 1])
+            );
         }
         if (flip_y) {
-            names.push(baseName + "_m_y" + parsed.ext);
-            writePromises.push(flipAndWrite(image, 0, names[names.length - 1]));
+            names.push(baseName + "_m_y" + item.parsed.ext);
+            writePromises.push(
+                flipAndWrite(item.image, 0, names[names.length - 1])
+            );
         }
         if (flip_x && flip_y) {
-            names.push(baseName + "_m_xy" + parsed.ext);
+            names.push(baseName + "_m_xy" + item.parsed.ext);
             writePromises.push(
-                flipAndWrite(image, -1, names[names.length - 1])
+                flipAndWrite(item.image, -1, names[names.length - 1])
             );
         }
 
