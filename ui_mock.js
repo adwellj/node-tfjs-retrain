0950e28b602118d0924ca0263cb40047cd2b64f5
adwellj <43648708+adwellj@users.noreply.github.com>
2018-12-26 07:49:02 -0500

    Removed refactored out param functions.

--- ui_mock.js
+++ ui_mock.js
@@ -1,30 +1,8 @@
 //@ts-check
-const getDenseUnits = () => {
-    return 100;
-};
-
-const getLearningRate = () => {
-    // Do not increase above 0.0001.
-    // Doing so has produced very poor training results.
-    return 0.0001;
-};
-
-const getBatchSizeFraction = () => {
-    return 0.4;
-};
-
-const getEpochs = () => {
-    return 50;
-};
-
 const trainStatus = status => {
     console.log(status);
 };
 
 module.exports = {
-    getDenseUnits,
-    getLearningRate,
-    getBatchSizeFraction,
-    getEpochs,
     trainStatus
 };
