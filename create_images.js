ffe92c7e6ad5de31ed5590dde9b0d1018ac73cba
adwellj <43648708+adwellj@users.noreply.github.com>
2019-03-14 09:16:35 -0400

    Updated to use sharp package.

--- create_images.js
+++ create_images.js
@@ -1,19 +1,22 @@
 // @ts-check
 const fse = require("fs-extra");
-const jpeg = require("jpeg-js");
+const sharp = require("sharp");
 const path = require("path");
 
 let imageDir;
-const image_width = 280;
-const image_height = image_width;
+const imgParams = {
+    width: 280,
+    height: 280,
+    channels: 3
+};
 const colors = [
-    { name: "red", rgb: [255, 0, 0] },
-    { name: "green", rgb: [0, 255, 0] },
-    { name: "blue", rgb: [0, 0, 255] },
-    { name: "cyan", rgb: [0, 255, 255] },
-    { name: "magenta", rgb: [255, 0, 255] },
-    { name: "yellow", rgb: [255, 255, 0] },
-    { name: "black", rgb: [0, 0, 0] }
+    { name: "red", rgb: [255, 20, 20] },
+    { name: "green", rgb: [20, 255, 20] },
+    { name: "blue", rgb: [20, 20, 255] },
+    { name: "cyan", rgb: [20, 255, 255] },
+    { name: "magenta", rgb: [255, 20, 255] },
+    { name: "yellow", rgb: [255, 255, 20] },
+    { name: "black", rgb: [20, 20, 20] }
 ];
 
 async function createAllImages() {
@@ -27,15 +30,30 @@ async function createColorImages(colorName, rgbArray, numToCreate) {
     await fse.ensureDir(dir);
 
     for (let index = 0; index < numToCreate; index++) {
-        let jpegImageData = encodeImage(rgbArray[0], rgbArray[1], rgbArray[2]);
         let filename = path.join(dir, `${colorName}_${index.toString()}.jpg`);
-        await fse.writeFile(filename, jpegImageData.data);
+        let img = createSharpImage(rgbArray[0], rgbArray[1], rgbArray[2]);
+        await img.toFile(filename);
     }
+}
+
+function createSharpImage(r, g, b) {
+    let imgData = Buffer.alloc(
+        imgParams.width * imgParams.height * imgParams.channels
+    );
+    let i = 0;
+
+    while (i < imgData.length) {
+        let color = randRGB(r, g, b);
+        imgData[i++] = color.r;
+        imgData[i++] = color.g;
+        imgData[i++] = color.b;
+    }
+    return sharp(imgData, { raw: imgParams });
 }
 
 function randRGB(r, g, b) {
     let val = Math.random();
-    if (val >= 0.25) {
+    if (val >= 0.1) {
         return {
             r: r * val,
             g: g * val,
@@ -48,26 +66,6 @@ function randRGB(r, g, b) {
             b: 255
         };
     }
-}
-
-function encodeImage(r, g, b) {
-    let frameData = Buffer.alloc(image_width * image_height * 4);
-    let i = 0;
-
-    while (i < frameData.length) {
-        let color = randRGB(r, g, b);
-        frameData[i++] = color.r; // red
-        frameData[i++] = color.g; // green
-        frameData[i++] = color.b; // blue
-        frameData[i++] = 0xff; // alpha - ignored in JPEGs
-    }
-
-    let rawImageData = {
-        data: frameData,
-        width: image_width,
-        height: image_height
-    };
-    return jpeg.encode(rawImageData, 75);
 }
 
 if (process.argv.length < 3) {
